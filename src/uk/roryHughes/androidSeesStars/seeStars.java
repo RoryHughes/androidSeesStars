db6381cdc4e94ba70ab40ff9356222af550eddb7
RoryHughes <rory.vans@gmail.com>
2011-02-15 15:23:13 +0000

    Set up project - nothing added, just auto generated stuff
    Signed-off-by: RoryHughes <rory.vans@gmail.com>

---
+++ src/uk/roryHughes/androidSeesStars/seeStars.java
@@ -0,0 +1,13 @@
+package uk.roryHughes.androidSeesStars;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class seeStars extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
